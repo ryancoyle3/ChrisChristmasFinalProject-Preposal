83f3c3a75bce10fe92d28d38b4c0ba821b69aadf
Stapletonchris <91710281+Stapletonchris@users.noreply.github.com>
2022-03-30 17:48:29 -0400

    Update Preposal.py

--- Preposal.py
+++ Preposal.py
@@ -2,12 +2,13 @@
 
 ## What idea(s) do you have for your final project?
 
-TODO
+"To create a blackjack simulator that will eventually become a mobile app or website."
 
 ## If you plan to collaborate with one or two classmates, what are their names?
 
-TODO
+"Vincent Schetroma Email: tuo46190@temple.edu"
+"Ryan Coyle Email: tuo69799@temple.edu"
 
 ## Do you have any questions of your own?
 
-TODO
+"Will we all submit our own project or should we edit the same repository."
