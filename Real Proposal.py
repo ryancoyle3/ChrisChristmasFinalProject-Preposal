41680f1ec0d006ac1fe6918927e8952d54afbe3d
Stapletonchris <91710281+Stapletonchris@users.noreply.github.com>
2022-04-06 21:46:20 -0400

    Add files via upload

---
+++ Real Proposal.py
@@ -0,0 +1,45 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+#Blackjack
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+#A game of blackjack, where you if you guess the riddle the dealer gives you, your hand will be perfect for the folloing round.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+#The software will be a game of blackjack that can be played by up to four people. What will make the game unique compared to a regular game of black jack is that there will be a series of riddles that the dealer will sneek in at the end of each round, and if you guess it correctly your next hand will be perfect.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+#N/A
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+# Vincent Schetroma Email: tuo46190@temple.edu
+# Ryan Coyle Email: tuo69799@temple.edu
+#They're TA is Jeremy Fan
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+#No matter what will will have a functioning blackjack game; however, if things go well we will try and create a mobile game.
+
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+#A Better outcome would be to animate the game using the LOVE download that you have listed on canvas.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+#The best outcome for our project would be to make and animated mobile IOS game.
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+#Fist I will need to formilerize myself with the .random function and everything that it is capibale of doing.
+#Then we will need to figure out how to all multiple plays to play at the same time.
+#Our current challenge is figuring out how to remove the cards from the deck once they have already been draw.
+#I am not exactle sure who will do what but one of us will need to work on animating and someone else will need to firgure out how we work with IOS.
