28cb0a58789d16897fdabe9229f7bc2e48539fed
Vstroma <102622193+Vstroma@users.noreply.github.com>
2022-04-12 18:01:36 -0400

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -1,3 +1,5 @@
+#Vincent Schetroma, Chris Christmas, Ryan Coyle
+
 import random
 
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
@@ -5,23 +7,15 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 deck = {0: "Ace", 1: "2", 2: "3", 3: "4", 4: "5", 5: "6", 6: "7", 7: "8",
          8: "9", 9: "10", 10: "Jack", 11: "Queen", 12: "King"}
 
+
+
 #Create an empty dictionary in the global space called cards delt, 
 #which tracks the cards delt
 #While not in cards delt pull from draw2
 #Phase1: deal one card to every player
 #Create a list players and use whiles to remove them once they stand
 
-def draw1():
-    hand = []
-    while len(hand) < 2:
-        cards = random.randint(0,12)
-        color = random.randint(0,3)
-        suit = suits[color]
-        card = deck[cards]
-        hand.append(cards)
-        hand.append(suit)
-    print(hand)
-#draw1()    
+
 
 def draw2():
     hand = []
@@ -42,17 +36,17 @@ def dealerDraw():
 
 #dealerDraw()
 
- 
+    
 def phase1():
-    one = []
-    while len(one) < 2:
+    Firstcard = []
+    while len(Firstcard) < 2:
         cards = random.randint(0,12)
         color = random.randint(0,3)
         suit = suits[color]
         card = deck[cards]
-        one.append(card)
-        one.append(suit)
-    print("The card is",one)
+        Firstcard.append(card)
+        Firstcard.append(suit)
+    print("The card is", Firstcard)
         
 def playAgain():
     choice = input("Enter 'Hit' to Hit or 'Stand' to Stand")
@@ -66,17 +60,7 @@ def playAgain():
     #else:
     # 3   print("no")
 
-#BET RULES
-#min bet amount must be divisible by $10 
 
-
-#END GAME
-#Ask for buy-in add to winnings and if winnings <= 0
-#Players name is removed from list    
-
-#If the dealers hand is 17 or greater the delater must stand
-#if the dealers hand is 16 or smaller the dealer must hit
-"""
 def BlackJack():
     phase1()
     done = False
@@ -85,29 +69,32 @@ def BlackJack():
         betAmount = input("How much are you willing to bet?")
         hand = draw2()
         dealersHand = dealerDraw()
-        handAmount = int(hand[0] + hand[2])
-        dealerAmount = dealersHand[0] + dealersHand[2]
+        handAmount = 18 #(int(hand[0]) + int(hand[2]))
+        dealerAmount = 17 #dealersHand[0] + dealersHand[2]
+        while dealerAmount <= 16:
+            addedCard = draw1()
+            dealerAmount += addedCard
+            if dealerAmount >= 17:
+                print("The Dealer stands at",dealerAmount)
+                continue
+            elif dealerAmount > 21:
+                print("The Dealer lost, you win.")
+                done = True
         if handAmount > 21:
             print("You lose")
             done = True
             playAgain()
-        elif handAmount = 21:
+        elif handAmount == 21:
             print("You Win!!\n", betAmount *2)
             done = True
             playAgain()
-        elif dealersAmount > 21:
+        elif dealerAmount > 21:
             winnings += betAmount * 2
             print("You win, ", betAmount * 2, "congratulations!")
             done = True
             playAgain()
 
         
-#BlackJack()
-"""
-
-#We draw two hard from the deck
-#onces cards are drawn they must be removed from the deck
-#Input print hit or stand
-#Repeat until over 21 or at 21
-#If hand > 21 you lose.
+#draw1()
+BlackJack()
 
